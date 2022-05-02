1a125155d00fd009194d23b799fbdecdb51942b6
Twilight <Twilight@qq.com>
2022-05-02 22:13:26 +0800

    push commit

--- src/main/java/com/atschool/git/GitTest.java
+++ src/main/java/com/atschool/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("Hello github2 hot fix!");
         System.out.println("Hello github2 冲突合并master!");
         System.out.println("Hello github2 冲突合并hot-fix!");
+        System.out.println("push test");
     }
 }
