f798d9ec8546440c5a731198a03f9c30333a4737
Twilight0720 <62397207+Twilight0720@users.noreply.github.com>
2022-05-02 22:22:10 +0800

    pull commit

--- src/main/java/com/atschool/git/GitTest.java
+++ src/main/java/com/atschool/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("Hello github2 冲突合并master!");
         System.out.println("Hello github2 冲突合并hot-fix!");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
