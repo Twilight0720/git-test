44fb0a36f3a0bc7b95f212d896fc485b1150315b
Twilight <Twilight@qq.com>
2022-05-02 17:15:00 +0800

    hot-fix commit1 whoo

--- src/main/java/com/atschool/git/GitTest.java
+++ src/main/java/com/atschool/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("Hello git!");
         System.out.println("Hello github!");
         System.out.println("Hello github2!");
+        System.out.println("Hello github2 hot fix!");
     }
 }
