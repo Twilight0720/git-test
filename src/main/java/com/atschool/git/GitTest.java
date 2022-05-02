5b856248a66e63a5dbb7276ff75fcc5db1372e18
Twilight <Twilight@qq.com>
2022-05-02 16:31:30 +0800

    first commit whoo

---
+++ src/main/java/com/atschool/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atschool.git;
+
+/**
+ * @author whoo
+ * @create 2022-05-02 16:27
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
